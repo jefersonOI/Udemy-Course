f74ddb08ca278764b8068daa1dbba1b3ae6d557d
jefersonOI <jeferson@oficinainteligente.com.br>
2025-05-05 23:12:39 -0300

    Sessao 3 aula 35

--- Course/Program.cs
+++ Course/Program.cs
@@ -9,18 +9,39 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.Write("Quantos numeros inteiros vocês quer digitar? ");
-            int quantidade = int.Parse(Console.ReadLine());
+            #region Exe1
+            Console.Write("Digite um numero entre 1 e 1000: ");
+            int num = int.Parse(Console.ReadLine());
 
-            int soma = 0;
-            for (int i = 1; i <= quantidade; i++)
+            for (int i = 1; i <= num; i++)
             {
-                Console.Write("Valor# " + i + ":");
-                int valor = int.Parse(Console.ReadLine());
-                soma += valor;
+                if (i % 2 == 1)
+                    Console.WriteLine(i);
             }
+            #endregion
 
-            Console.WriteLine("Total: " + soma);
+            #region Exe2
+            Console.Write("Digite um numero inteiro: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int IN = 0;
+            int OUT = 0;
+
+            for (int i = 0; i < num2; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+
+                if (x >= 10 && x <= 20)
+                    IN++;
+                else
+                    OUT++;
+            }
+            Console.WriteLine(IN + " in");
+            Console.WriteLine(OUT + " out");
+            #endregion
+
+
+
 
         }
     }
