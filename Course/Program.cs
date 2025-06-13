2a791d66f65078beb734348f3cadb452bcb9757d
jefersonOI <jeferson@oficinainteligente.com.br>
2025-06-13 19:37:43 -0300

    sessao 4 - aula 40 exec 1

--- Course/Program.cs
+++ Course/Program.cs
@@ -9,35 +9,23 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Triangulo x, y;
-            x = new Triangulo();
-            y = new Triangulo();
+            Pessoa x, y;
+            x = new Pessoa();
+            y = new Pessoa();
 
-            Console.WriteLine("Entre com as medidas do triangulo X");
-            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-
-            Console.WriteLine("Entre com as medidas do triangulo Y");
-            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            double p = (x.A + x.B + x.C) / 2.0;
-            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
-
-            p = (y.A + y.B + y.C) / 2.0;
-            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
-
-            Console.WriteLine("Area de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
-            Console.WriteLine("Area de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Dados da primeira Pessoa");
+            x.Nome = Console.ReadLine();
+            x.Idade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             
+            Console.WriteLine("Dados da segunda Pessoa");
+            y.Nome = Console.ReadLine();
+            y.Idade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            if (areaX > areaY)
-                Console.WriteLine("O maior é o X");
+            if (x.Idade > y.Idade)
+                Console.WriteLine("Pessoa mais velha: " + x.Nome);
             else
-                Console.WriteLine("O maior é o Y");
+                Console.WriteLine("Pessoa mais velha: " + y.Nome);
+                
         }
     }
 }
