cd477303da3f366c4bbf80137c003bb0e0e21caf
jefersonOI <jeferson@oficinainteligente.com.br>
2025-06-13 22:08:38 -0300

    sessa0 4 continuar a aula

--- Course/Program.cs
+++ Course/Program.cs
@@ -9,35 +9,7 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Produto oProduto = new Produto();
-
-            Console.WriteLine("Entre com os dados do Produto");
-            Console.Write("Nome: ");
-            oProduto.Nome = Console.ReadLine();
-            Console.Write("Preço: ");
-            oProduto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Quantidade: ");
-            oProduto.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Console.WriteLine("Dados do produto: " + oProduto);
-            Console.WriteLine();
-
-            #region Adicionando
-            Console.Write("Digite o número de produtos que será adiconado ao estoque: ");
-            int qtd = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            oProduto.AdicionarProdutos(qtd);
-            Console.WriteLine();
-            Console.WriteLine("Dados do produto atualizado: " + oProduto);
-            #endregion
-
-            #region Removendo
-            Console.WriteLine();
-            Console.Write("Digite o número de produtos que será removido ao estoque: ");
-            qtd = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            oProduto.RemoverProdutos(qtd);
-            Console.WriteLine();
-            Console.WriteLine("Dados do produto atualizado: " + oProduto);
-            #endregion
+            
         }
     }
 }
---
+++ Course/Retangulo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+
+        public double CalcularArea()
+        {
+            return (Largura * Altura);
+        }
+
+        public double CalcularPerimetro()
+        {
+            return (2 * (Largura + Altura));
+        }
+
+        public double CalcularDiagonal()
+        {
+            return Math.Sqrt((Largura * Largura) + (Altura * Altura));
+        }
+
+
+    }
+}
