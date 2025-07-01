0a24f190a276e9564dbbc9306987d2b169e1d867
jefersonOI <jeferson@oficinainteligente.com.br>
2025-06-30 21:13:56 -0300

    sessao 4 aula47 p1

---
+++ Course/Calculadora.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class Calculadora
+    {
+        public double PI = 3.14;
+
+        public double Circunferencia(double raio)
+        {
+            return 2.0 * PI * raio;
+        }
+
+        public double Volume(double raio)
+        {
+            return 4.0 / 3.0 * PI * raio * raio * raio;
+        }
+    }
+}
