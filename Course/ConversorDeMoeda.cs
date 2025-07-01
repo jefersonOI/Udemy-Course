e9323e4dda36a0b2e5330ace0f1f4af8e7454d7d
jefersonOI <jeferson@oficinainteligente.com.br>
2025-06-30 21:44:22 -0300

    sessao 4 aula48

---
+++ Course/ConversorDeMoeda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class ConversorDeMoeda
+    {
+        public static int ValorIOF = 6;
+
+        public static double CustoDolar(double cotacaoDolar, double valorAComprar)
+        {
+            double valorBrutoDolar = cotacaoDolar * valorAComprar;
+            return valorBrutoDolar + valorBrutoDolar * ValorIOF / 100.0;
+        }
+    }
+}
--- Course/Program.cs
+++ Course/Program.cs
@@ -9,15 +9,15 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Entre com o valor do raio");            
-            double raio = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o valor da cotação do dolar hoje: ");            
+            double valorDolarCotacao = double.Parse(Console.ReadLine());
 
-            double circunferencia = Calculadora.Circunferencia(raio);
-            double volume = Calculadora.Volume(raio);
 
-            Console.WriteLine("Circunferência: " + circunferencia.ToString("F2"));
-            Console.WriteLine("Volume: " + volume.ToString("F2"));
-            Console.WriteLine("Valor de PI: " + Calculadora.PI.ToString("F2"));
+            Console.WriteLine("Entre com o valor de dolares que será comprado: ");
+            double valorAComprar = double.Parse(Console.ReadLine());          
+
+
+            Console.WriteLine("Com a cotação do dolar esta sendo comprado por: " + ConversorDeMoeda.CustoDolar(valorDolarCotacao, valorAComprar).ToString("C2"));
         }
     }
 }
