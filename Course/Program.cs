149bd8f9497b1c300071477e9f11bb7109a2ba68
jefersonOI <jeferson@oficinainteligente.com.br>
2025-06-30 21:10:13 -0300

    sessao 4 aula46 exec1

--- Course/Program.cs
+++ Course/Program.cs
@@ -7,32 +7,31 @@ namespace Course
 {
     class Program
     {
+        static double PI = 3.14;
+
         static void Main(string[] args)
         {
-            Aluno oAluno = new Aluno();            
+            Console.WriteLine("Entre com o valor do raio");            
+            double raio = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Entre com os dados do Aluno");
-            Console.Write("Nome: ");
-            oAluno.Nome = Console.ReadLine();
-            
-            
-            Console.Write("Primeira Nota: ");
-            oAluno.Nota1 = Double.Parse(Console.ReadLine());
-            Console.Write("Segunda Nota: ");
-            oAluno.Nota2 = Double.Parse(Console.ReadLine());
-            Console.Write("Terceira Nota: ");
-            oAluno.Nota3 = Double.Parse(Console.ReadLine());
+            double circunferencia = Circunferencia(raio);
+            double volume = Volume(raio);
 
-
-            Console.WriteLine("Nota Final: " + oAluno.CalcularNotaFinal().ToString("F2"));
-
-            if (oAluno.AprovadoOuReprovado())
-                Console.WriteLine("APROVADO");
-            else
-            {
-                Console.WriteLine("REPROVADO");
-                Console.WriteLine("Faltaram " + oAluno.CalcularNotaFaltante().ToString("F2") + " Pontos");
-            }
+            Console.WriteLine("Circunferência: " + circunferencia.ToString("F2"));
+            Console.WriteLine("Volume: " + volume.ToString("F2"));
+            Console.WriteLine("Valor de PI: " + PI.ToString("F2"));
         }
+
+        static double Circunferencia(double raio)
+        {
+            return 2.0 * PI * raio;
+        }
+
+        static double Volume(double raio)
+        {
+            return 4.0/3.0 * PI * raio * raio * raio;
+        }
+
+
     }
 }
