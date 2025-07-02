0d93a83bff4c5b0fb28bc71692bf5ed92077610f
jefersonOI <jeferson@oficinainteligente.com.br>
2025-07-02 19:58:58 -0300

    sessao5 aula60

---
+++ Course/ContaCorrente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class ContaCorrente
+    {
+        public static double TaxaDoSaque = 5.0;
+
+        public int NumeroDaConta { get; private set; }
+        public string NomeTitular { get; set; }
+        public double SaldoContaCorrente { get; private set; }
+
+        public ContaCorrente(int numeroDaConta, string nomeTitular)
+        {
+            NumeroDaConta = numeroDaConta;
+            NomeTitular = nomeTitular;
+        }
+
+        public ContaCorrente(int numeroDaConta, string nomeTitular, double depositoInicial) : this(numeroDaConta, nomeTitular)
+        {
+            RealizarDepositdo(depositoInicial);
+        }
+
+        public void RealizarDepositdo(double valorDeposito)
+        {
+            SaldoContaCorrente += valorDeposito;
+        }
+
+        public void RealizarSaque(double valorSaque)
+        {
+            SaldoContaCorrente -= valorSaque + TaxaDoSaque;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + NumeroDaConta + ", Titular: " + NomeTitular + ", Saldo " + SaldoContaCorrente.ToString("C2");
+        }
+    }
+}
--- Course/Program.cs
+++ Course/Program.cs
@@ -8,13 +8,47 @@ namespace ourse
     {
         static void Main(string[] args)
         {
-            Produto oProduto = new Produto("TV", 500.00, 20);
 
-            Console.WriteLine(oProduto.Nome);
+            Console.Write("Entre com o numero da conta: ");
+            int numeroDaConta = int.Parse(Console.ReadLine());
+            
+            Console.Write("Entre com o nome do titular da conta: ");
+            string nomeTitular = Console.ReadLine();
+            
+            Console.Write("Havera deposito inicial (s/n): ");
+            char comDepositoInicial = char.Parse(Console.ReadLine());
+            
+            ContaCorrente oContaCorrente;
+            if (comDepositoInicial == 's')
+            {
+                Console.Write("Entre o valor para deposito inicial: ");
+                double saldoInicial = double.Parse(Console.ReadLine());
+                oContaCorrente = new ContaCorrente(numeroDaConta, nomeTitular, saldoInicial);
+            }
+            else
+                oContaCorrente = new ContaCorrente(numeroDaConta, nomeTitular);
 
-            oProduto.Nome = "T";
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(oContaCorrente);
+            Console.WriteLine();
 
-            Console.WriteLine(oProduto.Nome);
+            Console.Write("Entre com um valor para deposito: ");
+            double valorDeposito = double.Parse(Console.ReadLine());
+            oContaCorrente.RealizarDepositdo(valorDeposito);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(oContaCorrente);
+            Console.WriteLine();
+
+            Console.Write("Entre com um valor para saque: ");
+            double valorSaque = double.Parse(Console.ReadLine());
+            oContaCorrente.RealizarSaque(valorSaque);
+
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(oContaCorrente);
+            Console.WriteLine();
         }
     }
 }
